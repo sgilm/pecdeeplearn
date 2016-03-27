2bb94ae099536cdb853059769566a4e6f4485d60
Sam Gilmour <sgilmour07@gmail.com>
2016-03-27 22:08:41 +1300

    Minor changes

--- pecdeeplearn/experiments.py
+++ pecdeeplearn/experiments.py
@@ -12,40 +12,48 @@ def first():
     data_path = dt.read_data_path()
     volume = dt.load_volume(dt.list_volumes(data_path)[0])
     volume.switch_plane('axial')
-    train_vol = volume[250:300]
-    test_vol = volume[300]
+
+    train_vol = volume[100:150, 150:250]
+    test_vol = volume[150]
 
     train_vol.show_slice(0)
 
-    it = vt.BatchIterator(volume)
+    it = vt.BatchIterator(train_vol)
 
+    kernel_shape = [1, 9, 9]
     it.add_feature(
-        lambda volume, point: ft.patch(volume, point, [])
+        lambda volume, point: ft.patch(volume, point, kernel_shape)
+    )
+    it.add_feature(
+        lambda volume, point: ft.intensity_mean(volume, point, kernel_shape)
     )
 
-    # create Theano variables for input and target minibatch
-    input_var = T.tensor4('X', dtype='int32')
-    target_var = T.vector('y', dtype='int32')
+    # Create Theano variables for input and target minibatch.
+    input_var = T.matrix('X', dtype='float64')
+    target_var = T.vector('y', dtype='float64')
 
-    network = networks.basic(input_shape, input_var)
+    batch_size = 100
+    network = nt.basic((batch_size, it.get_input_size()), input_var)
 
-    # create loss function
+    # Create loss function.
     prediction = lasagne.layers.get_output(network)
     loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
-    loss = loss.mean() + 1e-4 * lasagne.regularization.regularize_network_params(
-            network, lasagne.regularization.l2)
+    loss = loss.mean() + \
+           1e-4 * lasagne.regularization.regularize_network_params(
+               network, lasagne.regularization.l2)
 
-    # create parameter update expressions
+    # Create parameter update expressions.
     params = lasagne.layers.get_all_params(network, trainable=True)
-    updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.01,
+    updates = lasagne.updates.nesterov_momentum(loss, params,
+                                                learning_rate=0.01,
                                                 momentum=0.9)
 
-    # compile training function that updates parameters and returns training loss
+    # Compile training function to update parameters and returns training loss.
     train_fn = theano.function([input_var, target_var], loss, updates=updates)
 
-    # train network (assuming you've got some training data in numpy arrays)
-    training_data = small_vol.iterate_batches(kernel_shape, 100)
-    for epoch in range(1):
+    # Train network.
+    training_data = it(100)
+    for epoch in range(5):
         loss = 0
         length = 0
         for input_batch, target_batch in training_data:
@@ -56,8 +64,11 @@ def first():
                 
         print("Epoch %d: Loss %g" % (epoch + 1, loss / length))
 
-    # use trained network for predictions
-    test_prediction = lasagne.layers.get_output(network, deterministic=True)
-    predict_fn = theano.function([input_var], T.argmax(test_prediction, axis=1))
-    ##print("Predicted class for first test input: %r" % predict_fn(test_data[0]))
+    # Use trained network for predictions.
+    # Test_prediction = lasagne.layers.get_output(network, deterministic=True)
+    # predict = theano.function([input_var], T.argmax(test_prediction, axis=1))
 
+
+if __name__ == '__main__':
+
+    first()
---
+++ pecdeeplearn/networks.py
@@ -0,0 +1,12 @@
+import lasagne
+import theano.tensor as T
+
+
+def basic(input_shape, input_var):
+
+    network = lasagne.layers.InputLayer(input_shape, input_var)
+    network = lasagne.layers.DenseLayer(network, num_units=200)
+    network = lasagne.layers.DenseLayer(network, num_units=2,
+                                        nonlinearity=T.nnet.softmax)
+
+    return network
