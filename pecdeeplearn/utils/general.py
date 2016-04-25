25912c24cf859d0000e8025021080e574af3597f
Sam Gilmour <sgilmour07@gmail.com>
2016-04-25 18:27:42 +1200

    Fixed relative reference to datapath.txt.

--- pecdeeplearn/utils/general.py
+++ pecdeeplearn/utils/general.py
@@ -1,11 +1,16 @@
 import pickle
+import os.path
 
 
 def datapath():
-    """Read and return the datapath specified in the root directory."""
+    """Read and return the datapath specified in the package root directory."""
+
+    # Get the path to the file containing the datapath.
+    full_filename = \
+        os.path.join(os.path.dirname(__file__), '..', 'datapath.txt')
 
     # Open and read the .txt file.
-    with open('../../datapath.txt', 'r') as f:
+    with open(full_filename, 'r') as f:
         path = f.read().strip()
 
     return path
