e6a87a34e57e197ae5c80c2f9b3e0dfd83775da4
Sam Gilmour <sgilmour07@gmail.com>
2016-05-11 21:04:37 +1200

    Added full map.

--- pecdeeplearn/extraction/maps.py
+++ pecdeeplearn/extraction/maps.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import numpy as np
 
 
@@ -6,7 +8,7 @@ def probability_bins(volumes, num_bins=25, scale=None):
     Create intensity bins and probabilities that a voxel in a bin is segmented.
 
     Args
-        volumes (list): a list of Volume objects that are to be used for
+        vols (list): a list of Volume objects that are to be used for
             creating the bins and probabilities.  All elements should be of the
             same size.
         num_bins (int): the number of bins to create.
@@ -29,7 +31,7 @@ def probability_bins(volumes, num_bins=25, scale=None):
     # Get the total number of voxels in each bin, and the bin boundaries.
     voxel_counts, bins = np.histogram(all_mri_data, bins=num_bins)
 
-    # Create a corresponding volumes with each element being a bin label.
+    # Create a corresponding vols with each element being a bin label.
     voxel_bin_indices = np.digitize(all_mri_data, bins)
 
     # Strip away only those elements that are segmented.
@@ -55,27 +57,27 @@ def probability_map(volume, bins, prob_bins):
     """Make a random training map from the likelihood that a voxel is a pec."""
 
     # Increase end of last bin so that the voxel of maximum intensity across
-    # all volumes used to create the map gets put into the final bin.
+    # all vols used to create the map gets put into the final bin.
     bins[-1] += 1
 
-    # Make a random volumes to use for selecting the voxels to train on.
+    # Make a random vols to use for selecting the voxels to train on.
     random_volume = np.random.random(volume.shape)
 
     # Assign each voxel its bin number.
     voxel_bin_ind = np.digitize(volume.mri_data, bins)
 
-    # Create a volumes where each voxel is replaced by the probability that it
+    # Create a vols where each voxel is replaced by the probability that it
     # is segmented (based only on its intensity value).
     prob_map = prob_bins[voxel_bin_ind.flatten() - 1].reshape(volume.shape)
 
-    # Threshold according to random volumes and voxel probabilities.
+    # Threshold according to random vols and voxel probabilities.
     prob_map[prob_map < random_volume] = 0
 
     return prob_map
 
 
 def segmentation_map(volumes):
-    """Create a training map for any segmented voxel of a list of volumes."""
+    """Create a training map for any segmented voxel of a list of vols."""
 
     # Count the number of times each voxel is segmented.
     counts = sum([volume.seg_data for volume in volumes])
@@ -116,6 +118,6 @@ def half_half_map(volume):
 
 
 def full_map(volume):
-    """Create a training map encompassing the whole of the input volume."""
+    """Create a training map encompassing the whole of the input vol."""
 
     return np.ones(volume.shape)
