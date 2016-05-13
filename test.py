d1dd5d8a06eae5715d79fea5c6773148902ba209
Sam Gilmour <sgilmour07@gmail.com>
2016-05-14 10:53:19 +1200

    General updates.

--- test.py
+++
@@ -1,7 +0,0 @@
-import pecdeeplearn as pdl
-import data_path
-
-if __name__ == '__main__':
-    exp = pdl.utils.Experiment(data_path.get(), 'single_hidden_layer')
-    x = exp.unpickle_volume('predicted_volume')
-    x.show_slice(0)
