e6dc7201b8cf66187176a206ecc6a27165076a51
Sam Gilmour <sgilmour07@gmail.com>
2016-05-17 14:25:00 +1200

    General fixes.

--- view_results.py
+++ view_results.py
@@ -3,11 +3,11 @@ import data_path
 
 if __name__ == '__main__':
 
-    experiment_name = 'single_conv_single_landmark'
+    experiment_name = 'single_conv_triple_landmark'
     exp = pdl.utils.Experiment(data_path.get(), experiment_name)
 
     test = exp.unpickle_volume('test_volume')
-    test.show_slice(0)
+    test.show_slice(0, slice_type='mri')
 
     pred =exp.unpickle_volume('predicted_volume')
     pred.show_slice(0)
